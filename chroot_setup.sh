b54871c2e18f972fc4c518afc0e0f10a41067884
Son1x-7732 <162944716+Son1x-7732@users.noreply.github.com>
2026-06-05 21:01:39 +0530

    Update chroot setup script for bootloader and configuration

--- chroot_setup.sh
+++ chroot_setup.sh
@@ -4,7 +4,7 @@ set -e
 echo "=== Setting Timezone and Locale ==="
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "arch-vm" > /etc/hostname
@@ -16,29 +16,28 @@ echo "zen0:password" | chpasswd
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 echo "=== Configuring Limine Bootloader ==="
-# Set up EFI boot binaries
-mkdir -p /efi/EFI/BOOT
-cp /usr/share/limine/BOOTX64.EFI /efi/EFI/BOOT/
+mkdir -p /boot/EFI/BOOT
+cp /usr/share/limine/BOOTX64.EFI /boot/EFI/BOOT/
 
-# Fetch dynamic UUID for the kernel parameter
 ROOT_UUID=$(blkid -s UUID -o value /dev/vda2)
 
-# Write the configuration
-cat << EOF > /efi/limine.conf
+cat << EOF > /boot/limine.conf
 timeout: 5
 
-/Arch Linux
+:Arch Linux
     protocol: linux
     kernel_path: boot():/vmlinuz-linux
     module_path: boot():/initramfs-linux.img
     cmdline: root=UUID=${ROOT_UUID} rw rootflags=subvol=@
+
+:Arch Linux (Fallback)
+    protocol: linux
+    kernel_path: boot():/vmlinuz-linux
+    module_path: boot():/initramfs-linux-fallback.img
+    cmdline: root=UUID=${ROOT_UUID} rw rootflags=subvol=@
 EOF
 
-# Move kernel files to FAT32
-cp /boot/vmlinuz-linux /efi/
-cp /boot/initramfs-linux.img /efi/
-
-echo "=== Executing Snapper Configuration Fix ==="
+echo "=== Configuring Snapper ==="
 umount /.snapshots
 rm -rf /.snapshots
 snapper -c root create-config /
@@ -47,6 +46,7 @@ mkdir /.snapshots
 mount -a
 
 echo "=== Enabling Display Manager ==="
+systemctl enable NetworkManager.service
 systemctl enable sddm.service
 
 exit
